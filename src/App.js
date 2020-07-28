1a5c075b75633533af4e8116b93e3b7b44bdd0b5
Leeseungjin <dltmdwls154@gmail.com>
2020-07-28 17:09:04 +0900

    vim error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Potato from "./Potato";
 
 function Food({ name, picture }) {
   return (
@@ -8,11 +9,12 @@ function Food({ name, picture }) {
     </div>
   );
 }
-// 음식 이름과 이미지를 가지고있는 array 를 선언
+
 const foodILike = [
   {
-    name: "kimchi",
-    image:"http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
   },
   {
     name: "Samgyeopsal",
@@ -35,14 +37,15 @@ const foodILike = [
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
-// map는 array의 각 item에 function(함수)를 실행
-// foodILike array의 각각의 아이템을 Food에 적용
+
 function App() {
   return (
     <div>
-      {foodILike.map(renderFood)}
+      <h1>Hello</h1>
+      {foodILike.map(dish => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
-
 export default App;
