4a2beff7ade74fed686db7883c4cb527caf385c9
Leeseungjin <dltmdwls154@gmail.com>
2020-08-02 13:34:27 +0900

    movie.js push

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    islLoading: true,
+    isLoading: true,
     movies: []
   };
-
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
   componentDidMount() {
-    setTimeout(() =>{
-      this.setState({islLoading: false});
-    }, 6000);
+    this.getMovies();
   }
-   render(){
-     const { islLoading } = this.state;
-   return <div>{islLoading ? "Loading" : "We are ready"}</div>;
-   }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
+  }
 }
-
 export default App;
